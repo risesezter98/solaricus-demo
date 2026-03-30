a94036da5a529a51506e176403a5c314704d27b3
risesezter98 <business@pfeifferdigital.de>
2026-03-30 16:53:12 +0200

    fix: Hero Bild links+Badges rechts, Grossanlage nicht mehr dunkel, Reveal sanfter

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
         observer.unobserve(entry.target);
       }
     });
-  }, { threshold: 0.15, rootMargin: '0px 0px -40px 0px' });
+  }, { threshold: 0.08, rootMargin: '0px 0px -20px 0px' });
 
   document.querySelectorAll('.reveal').forEach(function(el) {
     observer.observe(el);
